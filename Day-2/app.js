9a59bf286e5d0da59de18c5ff316ed388de77d99
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-11-19 14:18:00 -0300

    Para retirar o acomplamento que estava ocorrendo, foi implementado uma template strings dentro do alert, adicionando a variável número secreto.

--- Day-2/app.js
+++ Day-2/app.js
@@ -5,7 +5,7 @@ let chute = prompt('Escolha um número entre 1 e 10');
 
 // Se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai, você descobriu o número secreto (5)');
+    alert(`Isso ai, você descobriu o número secreto ${numeroSecreto}`);
     } else {
     alert('Você errou :c');
 }
