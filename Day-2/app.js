005717cb3f7183addf6a6bd1812f1eb0e884308d
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-11-20 19:15:25 -0300

    melhorado a lógica do app.js, adicionado tentativas.
    Realizado Desafio 1 ao 4 passado, utilizando loop while.

--- Day-2/app.js
+++ Day-2/app.js
@@ -1,11 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// Se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai, você descobriu o número secreto ${numeroSecreto}`);
+// Enquanto chute não for igual ao n. s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // Se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
-    alert('Você errou :c');
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
